e9b427c0953e1efa7180b54d9a4d0543b9ac1e3a
Vitor Wagner <vitormwagner@gmail.com>
2023-07-19 14:25:43 -0300

    Feat: :sparkles: Adds `fetch` function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,17 @@
-# Requisito 1 init
+# Requisito 1
+import requests
+import time
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+    except requests.exceptions.Timeout:
+        return None
+    if response.status_code == 200:
+        return response.text
+    return None
 
 
 # Requisito 2
