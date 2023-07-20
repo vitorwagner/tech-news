e9d96bdf966ad28b43fdbb66f2b66ee03abad7c0
Vitor Wagner <vitormwagner@gmail.com>
2023-07-20 16:26:20 -0300

    Feat: :sparkles: Adds `top_5_categories` function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,11 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news = find_news()
+    categories = {}
+    for item in news:
+        categories[item["category"]] = categories.get(item["category"], 0) + 1
+    categories = sorted(categories.items(), key=lambda x: (-x[1], x[0]))
+    return [category[0] for category in categories[:5]]
